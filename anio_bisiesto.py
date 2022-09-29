d2109cf89ea844ca67e91c3999fb359c0394b5bc
OVEGERO <75028114+OVEGERO@users.noreply.github.com>
2022-09-28 21:35:47 -0500

    Update anio_bisiesto.py

--- anio_bisiesto.py
+++ anio_bisiesto.py
@@ -1,6 +1,6 @@
 def anioBisiesto(anio):
 
-    if( anio%400 == 0 or (anio % 4 == 0 and not (anio % 100 == 0)) ):
+    if( anio % 400 == 0 or (anio % 4 == 0 and not (anio % 100 == 0)) ):
         print(f'El año {anio} es un año bisiesto')
     else:
         print(f'El año {anio} no es un año bisiesto')
