17b94c01978064f46291fce96e75c7e04334f1ec
OVEGERO <75028114+OVEGERO@users.noreply.github.com>
2022-09-30 18:18:14 -0500

    Add files via upload

---
+++ importacion_calculadora.py
@@ -0,0 +1,14 @@
+import operaciones.calculadora as calc
+
+calculadora = calc.Calculadora(2, 3)
+print("La suma es: ",calculadora.sumar())
+print("La resta es: ", calculadora.restar())
+print("La multiplicación es: ", calculadora.multiplicar())
+print("La división es: ", calculadora.dividir())
+
+
+
+
+
+
+
